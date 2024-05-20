32ec4856c2552c4beee6eed0c9a33cd8f61bd1c1
Dimitrios Georgousis <el19005@mail.ntua.gr>
2024-05-20 19:44:16 +0200

    code update

--- rabin_signature.py
+++ rabin_signature.py
@@ -1,11 +1,12 @@
 import sympy
 import random
 import hashlib
+import string
 
-#def _extended_gcd_recursive(a, b):
+# def _extended_gcd_recursive(a, b):
 #    def aux(a, b):
-#        if b == 0:
-#            return a, 1, 0 # a * 1 + b * 0 = a (base step of induction)
+#        if b == 0: (base step of induction)
+#            return a, 1, 0 # a * 1 + b * 0 = a
 #        else:
 #            gcd, x, y = aux(b, a % b) # b * x + (a % b) * y = gcd
 #                                      # (hypothesis of induction)
@@ -16,8 +17,9 @@ import hashlib
 #            # a * y + b * x - a * y + (a % b) * y = gcd
 #            # b * x + (a % b) * y = gcd, which is true
 #            return gcd, y, x - (a // b) * y
-#        
+#
 #    return aux(a, b)
+
 
 # Wikipedia algorithm for extended Euclidean algorithm
 def _extended_gcd(a, b):
@@ -30,8 +32,9 @@ def _extended_gcd(a, b):
         old_r, r = r, old_r - quotient * r
         old_s, s = s, old_s - quotient * s
         old_t, t = t, old_t - quotient * t
-    
+
     return old_r, old_s, old_t
+
 
 def _jacobi_symbol(a, n):
     if n < 0 or not n % 2:
@@ -44,7 +47,7 @@ def _jacobi_symbol(a, n):
         return int(n == 1)
     if n == 1 or a == 1:
         return 1
-    
+
     # if a and n are not coprime then Jacobi(a, n) = 0
     if sympy.igcd(a, n) != 1:
         return 0
@@ -63,42 +66,56 @@ def _jacobi_symbol(a, n):
         a %= n
     return j
 
+
 def _generate_rabin_prime(bits):
     while True:
-        p = sympy.randprime(2**(bits-1), 2**bits)
+        p = sympy.randprime(2 ** (bits - 1), 2**bits)
         if p % 4 == 3:
             return p
+
 
 def _hash_function(message):
     return int(hashlib.sha256(message.encode()).hexdigest(), 16)
 
+
+def _random_string(k):
+    """
+    Generate a random string of printable characters.
+    Input: k - the number of bits in the generated string
+    Output: a random string of printable characters
+    """
+    characters = k // 8
+    return "".join(random.choices(string.printable, k=characters))
+
+
 def key_generation(bits):
-    '''
+    """
     Generate a Rabin signature key pair.
     Input: bits - the number of bits in the primes p and q
     Output: n - the public key, (p, q) - the private key
-    '''
+    """
     p = _generate_rabin_prime(bits)
     q = _generate_rabin_prime(bits)
     while p == q:
         q = _generate_rabin_prime(bits)
-    n = p*q
-    
+    n = p * q
+
     return n, (p, q)
 
-def sign(message, private_key, k=128):
-    '''
+
+def sign(message, private_key, k=256):
+    """
     Sign a message using the Rabin signature scheme.
     Input: message - the message to sign,
            private_key - the private key,
            k - the number of random bits to use
     Output: (x, u) - the signature
-    '''
+    """
     p, q = private_key
-    n = p*q
+    n = p * q
 
     while True:
-        u = str(random.getrandbits(k))
+        u = _random_string(k)
         c = _hash_function(message + u) % n
 
         # check x^2 = c mod n, this will be true iff
@@ -116,29 +133,32 @@ def sign(message, private_key, k=128):
         # b = 0 mod p, b = 1 mod q
         # x = x_p * a + x_q * b mod n, so
         # x = x_p * q * y_q + x_q * p * y_p mod n
-        x_p = pow(c, (p+1)//4, p) # known formula
-        x_q = pow(c, (q+1)//4, q) # known formula
+        x_p = pow(c, (p + 1) // 4, p)  # known formula
+        x_q = pow(c, (q + 1) // 4, q)  # known formula
 
         _, y_p, y_q = _extended_gcd(p, q)
-        x = (x_p*q*y_q + x_q*p*y_p) % n
+        x = (x_p * q * y_q + x_q * p * y_p) % n
         return x, u
 
+
 def verify(message, signature, public_key):
-    '''
+    """
     Verify a message using the Rabin signature scheme.
     Input: message - the message to verify,
            signature - the signature,
            public_key - the public key
     Output: True if the signature is valid, False otherwise
-    '''
+    """
     n = public_key
     x, u = signature
     c = _hash_function(message + u) % n
     return pow(x, 2, n) == c
 
+
 def _main():
+
     bits = 512
-    k = 60
+    k = 64
     message = str(input("Enter a message: "))
 
     n, private_key = key_generation(bits)
@@ -149,6 +169,7 @@ def _main():
     print("Signature:", signature)
 
     print("Verification:", verify(message, signature, n))
+
 
 if __name__ == "__main__":
     _main()
--- test.py
+++ test.py
@@ -2,14 +2,15 @@ import random
 import unittest
 from rabin_signature import key_generation, sign, verify
 
+
 class TestRabinSignature(unittest.TestCase):
     def setUp(self):
         self.message_list = [
             "lore ipsum",
             "hello world",
             "this is a test",
-            "a"*1000,
-            "b"*1000,
+            "a" * 1000,
+            "b" * 1000,
             "what's the answer to life, the universe, and everything?",
             "42",
             "1+1=2",
@@ -18,14 +19,16 @@ class TestRabinSignature(unittest.TestCase):
             "This message is top secret!",
         ]
         self.bits = 512
-        self.k = 60
+        self.k = 64
 
     def test_key_generation(self):
         for _ in range(10):
             n, private_key = key_generation(self.bits)
 
-            self.assertIsInstance(private_key, tuple)  # Check if private_key is a tuple
-            self.assertEqual(len(private_key), 2)  # Check if private_key has 2 elements
+            # Check if private_key is a tuple
+            self.assertIsInstance(private_key, tuple)
+            # Check if private_key has 2 elements
+            self.assertEqual(len(private_key), 2)
 
     def test_sign_and_verify(self):
         for _ in range(10):
@@ -33,9 +36,15 @@ class TestRabinSignature(unittest.TestCase):
                 n, private_key = key_generation(self.bits)
                 public_key = n
                 signature = sign(message, private_key, self.k)
-                self.assertIsInstance(signature, tuple) # Check if signature is a tuple
-                self.assertEqual(len(signature), 2) # Check if signature has 2 elements
-                self.assertTrue(verify(message, signature, public_key))  # Check if signature is valid
+                # Check if signature is a tuple
+                self.assertIsInstance(signature, tuple)
+
+                # Check if signature has 2 elements
+                self.assertEqual(len(signature), 2)
+                self.assertTrue(
+                    verify(message, signature, public_key)
+                )  # Check if signature is valid
+
 
 if __name__ == "__main__":
     random.seed(17)
